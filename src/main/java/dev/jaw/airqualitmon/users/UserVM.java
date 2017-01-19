74feedf08b7a1da768a64227ba2b22c79c90d5aa
Aqudei <fdeu.aalmine@gmail.com>
2017-01-19 18:26:13 +0800

    jj

---
+++ src/main/java/dev/jaw/airqualitmon/users/UserVM.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dev.jaw.airqualitmon.users;
+
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author Archie
+ */
+public class UserVM {
+
+    public SimpleStringProperty usernameProperty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public SimpleStringProperty passwordProperty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public SimpleStringProperty passwordCopyProperty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/main/java/dev/jaw/airqualitmon/users/UsersView.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dev.jaw.airqualitmon.users;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+/**
+ *
+ * @author Archie
+ */
+public class UsersView extends FXMLView {
+    
+}
